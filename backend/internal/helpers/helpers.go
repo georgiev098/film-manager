e3bc2fc9b34982b7804372468f0e3afd8754cc4e
Daniel Georgiev <georgiev.daniel@outlook.com>
2026-02-02 19:16:31 +0200

    (feat) implement read & write JSON helpers

--- backend/internal/helpers/helpers.go
+++ backend/internal/helpers/helpers.go
@@ -1,6 +1,13 @@
 package helpers
 
-import "fmt"
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"maps"
+	"net/http"
+)
 
 // helper to convert string env vars to int
 func AtoiOrDefault(s string, def int) int {
@@ -10,4 +17,42 @@ func AtoiOrDefault(s string, def int) int {
 		return def
 	}
 	return val
+}
+
+// ReadJSON decodes JSON from an HTTP request body into dst.
+// dst must be a pointer.
+func ReadJSON(w http.ResponseWriter, r *http.Request, dst any) error {
+	const maxBytes = 1 << 20 // 1MB
+
+	r.Body = http.MaxBytesReader(w, r.Body, maxBytes)
+	defer r.Body.Close()
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	if err := dec.Decode(dst); err != nil {
+		return err
+	}
+
+	// Catch multiple JSON values
+	if err := dec.Decode(&struct{}{}); err != io.EOF {
+		return errors.New("body must contain only a single JSON value")
+	}
+
+	return nil
+}
+
+// WriteJSON writes v as JSON to the HTTP response.
+func WriteJSON(
+	w http.ResponseWriter,
+	status int,
+	data any,
+	headers http.Header,
+) error {
+	maps.Copy(w.Header(), headers)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+
+	return json.NewEncoder(w).Encode(data)
 }
